08b98e0b2efc63dcf52e43e4f0a3f558f5092368
Kix Panganiban <panganiban@outlook.com>
2013-02-07 22:47:06 +0800

    First version.

---
+++ readme.md
@@ -0,0 +1,14 @@
+soUPerfind
+==========
+
+UPCAT results crawler using Python and BeautifulSoup.
+
+Usage
+-----
+Simply run **python souperfind.py** and enter search query. Query may be a partial of the student's name, student id, campus, or degree program.
+
+Requirements
+------------
+soUPerfind requires _Python 2.7_ with the _BeautifulSoup_ module installed. For instructions on installing BeautifulSoup, you may visit [BeautifulSoup's homepage](http://www.crummy.com/software/BeautifulSoup/#Download).
+
+NOTE: Due to a required Python module being named differently in Python 2.7 and 3+, soUPerfind only supports Python 2.7 for now.
---
+++ souperfind.py
@@ -0,0 +1,135 @@
+"""
+soUPerfind 0.1
+UPCAT Results Search Engine
+(c) Kix Panganiban 2011
+Requires:
+Python 2.7 and BeautifulSoup 4.0
+"""
+
+from bs4 import BeautifulSoup
+import httplib
+import re
+import threading
+
+
+"""
+The main crawler thread class.
+"""
+class crawler(threading.Thread):
+	def __init__(self, cid, resultset, totalEntries, totalResults, running):
+		self._id = cid
+		self._resultset = resultset 
+		self._entries = totalEntries
+		self._results = totalResults
+		self._running = running
+		self._links = []
+		threading.Thread.__init__(self)
+
+	# Adds a link to crawl in crawler queue
+	def addLink(self,link):
+		self._links.append(link)
+
+	def run(self):
+		print "Crawler #" + str(self._id) + " running!"
+		for link in self._links:
+			try:
+				self._crawlConn = httplib.HTTPConnection('upcat.up.edu.ph')
+				self._crawlConn.request('GET', '/results/'+link)
+				self._pageRes = self._crawlConn.getresponse()
+			except:
+				print "Error retrieving results at: http://upcat.up.edu.ph/results/"+link
+			self._pageData = self._pageRes.read()
+			self._pageSoup = BeautifulSoup(self._pageData)
+			
+			# Parse and store data
+			tableItem = self._pageSoup.find("table", class_='printable')
+			tdItem = tableItem.find_all("td", class_='printable')
+			itemType = 0
+			# initialize current student array
+			currStudent = [" ", " ", " ", " "]	
+			for itemContent in tdItem:
+				currStudent[itemType] = itemContent.get_text()
+				if itemType == 3:
+					itemType = 0
+					# if query is found in any of the four fields
+					if (re.search(query, currStudent[0], re.IGNORECASE)) or (re.search(query, currStudent[1], re.IGNORECASE)) or (re.search(query, currStudent[2], re.IGNORECASE)) or (re.search(query, currStudent[3], re.IGNORECASE)):
+						print "\nMatch: \n" + currStudent[0] + "\n" + currStudent[1] + "\n" + currStudent[2] + "\n" + currStudent[3]
+						self._resultset.append([currStudent[0], currStudent[1], currStudent[2], currStudent[3]])
+						self._results += 1
+					self._entries += 1
+				else:
+					itemType += 1
+			print "Crawler #" +str(self._id) + ": http://upcat.up.edu.ph/results/"+link+" done."
+			self._running -= 1
+
+		print "Crawler #" + str(self._id) + " done!"
+
+
+"""
+Main execution line.
+"""
+print "-------------------------------"
+print "soUPerfind: UPCAT Search Engine" 
+print "using Python and BeautifulSoup"
+print "-------------------------------"
+print "Connecting...",
+# Connect to UPCAT results server
+try:
+	mainConn = httplib.HTTPConnection('upcat.up.edu.ph')
+	mainConn.request('GET', '/results/')
+	mainRes = mainConn.getresponse()
+except:
+	print "Error connecting to UPCAT server."
+	exit()
+mainData = mainRes.read()
+print " OK"
+
+# Invokes BeautifulSoup on UPCAT mainpage
+mainSoup = BeautifulSoup(mainData)
+
+print "Locating listing pages...",
+# Mainpage link array
+mainLinks = []
+for link in mainSoup.find_all('a'):
+	if 'page' in link.get('href'):
+		mainLinks.append(link.get('href'))
+print " OK"
+query = raw_input("Enter search query: ")
+query.upper()
+print "\nSearching (10 active crawlers)..."
+
+# Initialize results storage
+resultSet = []
+results = 0
+entries = 0
+
+# Crawler array
+crawlers = []
+crawlerId = 0
+
+# Running crawlers counter
+running = 0
+
+# Initialize crawlers
+for i in range(0,10):
+	newCrawler = crawler(i, resultSet, entries, results, running)
+	crawlers.append(newCrawler)
+
+# Set crawlers free!
+for link in mainLinks:
+	crawlers[crawlerId].addLink(link)
+
+	if not crawlers[crawlerId].isAlive():
+		crawlers[crawlerId].start()
+	
+	if crawlerId == 9:
+		crawlerId = 0
+	else:
+		crawlerId += 1
+
+	running += 1
+
+while(True):
+	if running == 0:
+		print "Done. " + str(entries) + " entries searched; " + str(results) + " matches."
+		break
